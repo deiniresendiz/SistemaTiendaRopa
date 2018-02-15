8a3998266630b14de453c07979e6153b6662d9a1
Unknown <rootn66@gmail.com>
2018-02-15 00:37:36 -0600

    Clientes
    catalogos termindo

--- SistemaVenta/My Project/Application.Designer.vb
+++ SistemaVenta/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SistemaVenta.frmCategorias
+            Me.MainForm = Global.SistemaVenta.frmClientes
         End Sub
     End Class
 End Namespace
